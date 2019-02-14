608e7c41f5d72ef8b330522635b81f2a519b21c3
Michael Nguyen <michaelnguyen974@gmail.com>
2019-02-14 16:13:43 +0000

    created three tests

--- js-jasmine/spec/gilded_rose_spec.js
+++ js-jasmine/spec/gilded_rose_spec.js
@@ -1,10 +1,35 @@
-var {Shop, Item} = require('../src/gilded_rose.js');
+// var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
+var shop;
+  beforeEach( () => {
+    shop = new Shop
+  })
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+  it("looks for aged brie in Item object via its property name", function() {
+    const gildedRose = new Shop([ new Item("Aged Brie", 0, 0) ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].name).toEqual("Aged Brie");
   });
+
+  it("will check for property sellIn to be an integer", () => {
+    var item = [new Item("Could be any food", 0, 0)]
+    const gildedRose = new Shop(item);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(0)
+  })
+
+  it("will check for property sellIn to be an integer", () => {
+    var item = [new Item("Could be any food", 0, 2)]
+    const gildedRose = new Shop(item);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(0)
+  })
+
+  it("will check for property sellIn to be an integer", () => {
+    var item = [new Item("Aged Brie", 0, 0)]
+    const gildedRose = new Shop(item);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(2)
+  })
 
 });
