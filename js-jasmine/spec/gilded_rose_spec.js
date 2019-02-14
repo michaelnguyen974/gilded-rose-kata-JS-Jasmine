f61ee66cf417f640b511e55d73bcdc3841bc0359
Michael Nguyen <michaelnguyen974@gmail.com>
2019-02-14 16:52:56 +0000

    removed mocha files

--- js-jasmine/spec/gilded_rose_spec.js
+++ js-jasmine/spec/gilded_rose_spec.js
@@ -11,21 +11,21 @@ var shop;
     expect(items[0].name).toEqual("Aged Brie");
   });
 
-  it("will check for property sellIn to be an integer", () => {
+  it("Any food will have a quality of 0 when SellIn date is 0", () => {
     var item = [new Item("Could be any food", 0, 0)]
     const gildedRose = new Shop(item);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toEqual(0)
   })
 
-  it("will check for property sellIn to be an integer", () => {
+  it("any food that has a SellIn of 0 apart from aged brie will automatically have a quality of 0", () => {
     var item = [new Item("Could be any food", 0, 2)]
     const gildedRose = new Shop(item);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toEqual(0)
   })
 
-  it("will check for property sellIn to be an integer", () => {
+  it("Aged brie will increase quality when SellIn date is reached", () => {
     var item = [new Item("Aged Brie", 0, 0)]
     const gildedRose = new Shop(item);
     const items = gildedRose.updateQuality();
